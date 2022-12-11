0ac6865c7a213cae26e23525970d3a92fa3d1691
kndi9 <87029571+kndi9@users.noreply.github.com>
2022-12-11 15:58:59 -0500

    Modified javascript

--- js/main.js
+++ js/main.js
@@ -1,7 +1,5 @@
 document.addEventListener('DOMContentLoaded', function () {
-    var navbar = document.querySelector('.navbar');
-
     document.getElementById('navbar-toggle').addEventListener('click', function (e) {
-        navbar.classList.toggle('open');
+        document.querySelector('.navbar').classList.toggle('open');
     });
 });
